e94670a01471b2c3a2fc688ac795cde83e162b16
Michel Vuijlsteke <michel@zog.org>
2018-04-01 11:06:31 +0200

    nieuwe teksten

--- markov.py
+++ markov.py
@@ -1,16 +1,19 @@
 import markovify
 
 # Get raw text as string.
-with open("source/nvagent2012.txt") as f:
+with open("source/vb2014.txt") as f:
     textA = f.read()
-with open("source/comm.txt") as f:
+with open("source/spagroen2012.txt") as f:
     textB = f.read()
+with open("source/nvagent2012.txt") as f:
+    textC = f.read()
 
 # Build the model.
 textA_model = markovify.Text(textA)
 textB_model = markovify.Text(textB)
+textC_model = markovify.Text(textC)
 
-model_combo = markovify.combine([ textA_model, textB_model ], [ 1, 1 ])
+model_combo = markovify.combine([ textA_model, textB_model, textC_model ], [ 1, 1, 1 ])
 
 
 for i in range(30):
